4586a65a1f97338035fe7598dc90d30ee5e3fb76
liberbell <liberbell@gmail.com>
2018-09-10 08:59:17 +0900

    for2

--- for1.py
+++ for1.py
@@ -1,4 +1,11 @@
-words = ['cat', 'window', 'defenestrate']
-
-for w in words:
-    print(w,len(w))
+# words = ['cat', 'window', 'defenestrate']
+#
+# for w in words:
+#     print(w,len(w))
+for n in range(2, 1000):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, 'is a prime number')
