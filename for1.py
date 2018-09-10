e6e167931086d8777dbb280fb9b330f3a1b23fd9
liberbell <liberbell@gmail.com>
2018-09-10 09:21:14 +0900

    for3

--- for1.py
+++ for1.py
@@ -6,6 +6,7 @@ for n in range(2, 1000):
     for x in range(2, n):
         if n % x == 0:
             print(n, 'equals', x, '*', n//x)
+            # break roop
             break
     else:
         print(n, 'is a prime number')
