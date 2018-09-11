0bb27385cfc43650ef17c6a60d49c579c97e5df3
liberbell <liberbell@gmail.com>
2018-09-11 13:25:47 +0900

    input4

--- input.py
+++ input.py
@@ -6,6 +6,13 @@ URL = input('>> ')
 
 if re.match(r"^https?:\/\/", URL):
     print(URL)
+    try:
+        res = urllib.request.urlopen(URL)
+        res.close()
+    except urllib.request.HTTPError as reqerr:
+        print('Not found: ', URL)
+        print('Error Code: ' + reqerr.code)
+
 else:
     print('Wrong URL')
 
