47f988acd4da26adc71ff5f8759f4124fa5824f6
liberbell <liberbell@gmail.com>
2018-09-12 07:58:00 +0900

    urllib2

--- urllib1.py
+++ urllib1.py
@@ -4,4 +4,11 @@ def main():
     url = 'http://httpbin.org/xml'
 
     result = urllib.request.urlopen(url)
-    print('Result code: {0}'.format.(result.status))
+    print('Result code: {0}'.format(result.status))
+
+    print('Headers: ------------------')
+
+    print('Returned data: ------------')
+
+if __name__ == '__main__':
+    main()
