b273114dc4adb085f0fd2e38ee71eafa29a8a6c8
liberbell <liberbell@gmail.com>
2018-09-11 09:29:52 +0900

    input4

--- input.py
+++ input.py
@@ -1,10 +1,14 @@
 import urllib.request
+import re
 
 print('Enter URL')
-
 URL = input('>> ')
-print(URL)
 
-with urllib.request.urlopen(URL) as res:
-   html = res.read().decode("utf-8")
-   print(html)
+if re.match(r"^https?:\/\/", URL):
+    print(URL)
+else:
+    print('Wrong URL')
+
+# with urllib.request.urlopen(URL) as res:
+#    html = res.read().decode("utf-8")
+#    print(html)
