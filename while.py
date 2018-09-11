90b63f1052bb1660c4296c0885aba1889033e814
liberbell <liberbell@gmail.com>
2018-09-11 09:09:44 +0900

    while2

--- while.py
+++ while.py
@@ -9,3 +9,7 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
         if retries < 0:
             raise ValueError('invalid user response')
         print(reminder)
+
+ask_ok('Do you really want to quit?')
+ask_ok('OK to overwrite the file?', 2)
+ask_ok('OK to overwrite the file?', 2, 'Come on, only yes or no!')
