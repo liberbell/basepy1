25d34dbb44b30c2e0cbd553f306146af92213834
liberbell <liberbell@gmail.com>
2018-09-13 07:49:11 +0900

    urldata3

--- urldata.py
+++ urldata.py
@@ -1,7 +1,19 @@
 import urllib.request
+import urllib.parse
 
 def main():
-    url = 'http://httpbin.org/xml'
+    url = 'http://httpbin.org/get'
+
+    args = {
+        'name': 'Joe Marini',
+        'is_auther': True,
+    }
+
+
+    result = urllib.request.urlopen(url)
+
+    print('Result code: {0}'.format(result.status))
+    print('Returned data: ------------------')
 
 
 
