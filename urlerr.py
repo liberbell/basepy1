16b07b204d7aea5dda8592e5713464d4c7bf88c9
liberbell <liberbell@gmail.com>
2018-09-14 08:39:40 +0900

    urlerr6

--- urlerr.py
+++ urlerr.py
@@ -1,15 +1,20 @@
 import urllib.request
 from http import HTTPStatus
+from urllib.error import HTTPError
 
 def main():
     # url = 'http://httpbin.org/html'
     url = 'http://httpbin.org/status/404'
     # url = 'http://no-such-server.org'
 
-    result = urllib.request.urlopen(url)
-    print('Result Code: {0}'.format(result.status))
-    if (result.getcode() == HTTPStatus.OK):
-        print(result.read().decode('utf-8'))
+    try:
+        result = urllib.request.urlopen(url)
+        print('Result Code: {0}'.format(result.status))
+        if (result.getcode() == HTTPStatus.OK):
+            print(result.read().decode('utf-8'))
+
+    except HTTPError as err:
+        print('Error: {0}'.format.err.code)
 
 
 if __name__ == '__main__':
