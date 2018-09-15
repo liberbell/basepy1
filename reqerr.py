c3521f23aed7e53d3ec3ceada75c3798bbae56a2
liberbell <liberbell@gmail.com>
2018-09-15 23:12:38 +0900

    reqerr6

--- reqerr.py
+++ reqerr.py
@@ -3,13 +3,17 @@ from requests import HTTPError, Timeout
 
 def main():
     try:
-        url = 'http://httpbin.org/status/404'
+        # url = 'http://httpbin.org/status/404'
+        url = 'http://httpbin.org/delay/5'
         result = requests.get(url, Timeout=2)
         result.raise_for_status()
         printResults(result)
 
     except HTTPError as err:
         print('Error: {0}'.format(err))
+
+    except Timeout as err:
+        print('Request Time out: {0}'.format(err))
 
 def printResults(resData):
     print('Result code: {0}'.format(resData.status_code))
