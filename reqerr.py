2cdeb628ef2b287d7bfca32bd0f11641fac64778
liberbell <liberbell@gmail.com>
2018-09-15 22:47:03 +0900

    reqerr2

---
+++ reqerr.py
@@ -0,0 +1,30 @@
+import requests
+
+def main():
+    url = 'http://httpbin.org/status/404'
+    result = requests.get(url)
+    printResults(result)
+
+def printResults(resData):
+    print('Result code: {0}'.format(result.status))
+    print('\n')
+
+    print('Returned data: ------------')
+    print(resData.text)
+
+
+    # try:
+    #     result = urllib.request.urlopen(url)
+    #     print('Result Code: {0}'.format(result.status))
+    #     if (result.getcode() == HTTPStatus.OK):
+    #         print(result.read().decode('utf-8'))
+    #
+    # except HTTPError as err:
+    #     print('Error: {0}'.format(err.code))
+    #
+    # except URLError as err:
+    #     print ('Yeah that server is bunk. {0}'.format(err.reason))
+
+
+if __name__ == '__main__':
+        main()
