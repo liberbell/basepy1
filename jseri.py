57d02a6d9b75773dbcd7d9087b79b45d2f1ae7e1
liberbell <liberbell@gmail.com>
2018-09-16 10:01:04 +0900

    jseri1

---
+++ jseri.py
@@ -0,0 +1,30 @@
+import json
+
+def main():
+    jsonStr = '''{
+        "sandwitch" : "Reuben",
+        "toasted" : true,
+        "toppings" : [
+            "Thousasnd Island Dressing",
+            "Sauerkraut",
+            "Pickles"
+        ],
+        "Price" : 8.99
+    }'''
+
+    jsonStr = json.dumps(pythonData)
+
+    print('JSON Data: ------------------')
+    print(jsonStr)
+
+    # data = json.loads(jsonStr)
+    #
+    # print('Sandwitch: ' + data['sandwitch'])
+    # if (data['toasted']):
+    #     print(('And it`s toasted!'))
+    #
+    # for topping in data['toppings']:
+    #     print('Topping: ' + topping)
+
+if __name__ == '__main__':
+    main()
