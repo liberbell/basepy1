944ec397891c5a2d63b33aef9b2c28b1a4d7ba2e
liberbell <liberbell@gmail.com>
2018-09-16 09:43:36 +0900

    jparse3

--- jparse.py
+++ jparse.py
@@ -12,6 +12,16 @@ def main():
         'Price' : 8.99
     }'''
 
+    data = json.loads(jsonStr)
+
+    print('Sandwitch: ' + data['sandwitch'])
+    if (data['toasted']):
+        print(('And it's toasted!'))
+
+    for topping in data['toppings']:
+        print('Toppings: ' + topping)
+
+
 
 if __name__ == '__main__':
     main()
