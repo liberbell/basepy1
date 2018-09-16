197f2098f4b2991ed6e65d97d308aed3d298fc9b
liberbell <liberbell@gmail.com>
2018-09-16 09:49:39 +0900

    jparse4

--- jparse.py
+++ jparse.py
@@ -2,26 +2,24 @@ import json
 
 def main():
     jsonStr = '''{
-        'sandwitch' : 'Reuben',
-        'toasted' : true,
-        'toppings' : [
-            'Thousasnd Island Dressing',
-            'Sauerkraut',
-            'Pickles'
+        "sandwitch" : "Reuben",
+        "toasted" : true,
+        "toppings" : [
+            "Thousasnd Island Dressing",
+            "Sauerkraut",
+            "Pickles"
         ],
-        'Price' : 8.99
+        "Price" : 8.99
     }'''
 
     data = json.loads(jsonStr)
 
     print('Sandwitch: ' + data['sandwitch'])
     if (data['toasted']):
-        print(('And it's toasted!'))
+        print(('And it`s toasted!'))
 
     for topping in data['toppings']:
-        print('Toppings: ' + topping)
-
-
+        print('Topping: ' + topping)
 
 if __name__ == '__main__':
     main()
