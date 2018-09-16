bcf699f1fa6a9913b26a50907cbeeba0919024d9
liberbell <liberbell@gmail.com>
2018-09-16 09:39:10 +0900

    jparse2

---
+++ jparse.py
@@ -0,0 +1,17 @@
+import json
+
+def main():
+    jsonStr = '''{
+        'sandwitch' : 'Reuben',
+        'toasted' : true,
+        'toppings' : [
+            'Thousasnd Island Dressing',
+            'Sauerkraut',
+            'Pickles'
+        ],
+        'Price' : 8.99
+    }'''
+
+
+if __name__ == '__main__':
+    main()
