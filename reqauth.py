547d5aa373be12bf409071844ba364ea281cd4d6
liberbell <liberbell@gmail.com>
2018-09-16 09:26:47 +0900

    reqauth3

--- reqauth.py
+++ reqauth.py
@@ -3,7 +3,7 @@ import requests
 def main():
     url = 'http://httpbin.org/basic-auth/JoeMarini/Password'
 
-    printResults(url)
+    printResults(result)
 
 def printResults(resData):
     print('Result code: {0}'.format(resData.status_code))
