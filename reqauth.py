e6235e89d6650ef9c4a5216a248da1fcc0316486
liberbell <liberbell@gmail.com>
2018-09-16 09:25:50 +0900

    reqauth2

---
+++ reqauth.py
@@ -0,0 +1,16 @@
+import requests
+
+def main():
+    url = 'http://httpbin.org/basic-auth/JoeMarini/Password'
+
+    printResults(url)
+
+def printResults(resData):
+    print('Result code: {0}'.format(resData.status_code))
+    print('\n')
+
+    print('Returned data--------------------')
+    print(resData.text)
+
+if __name__ == '__main__':
+        main()
