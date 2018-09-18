73eac4268116b52d1228330142651c98893c48f3
liberbell <liberbell@gmail.com>
2018-09-19 07:59:49 +0900

    domparse5

--- domparse.py
+++ domparse.py
@@ -14,6 +14,15 @@ def main():
     items = domtree.getElementsByTagName("item")
     print('There are {0} item tags'.format(items.length))
 
+    newItem = domtree.createElement('item')
+
+    newItem.appendChild(domtree.createTextNode('This is some text'))
+    firstSlide = domtree.getElementsByTagName('slide')[0]
+
+    firstSlide.appendChild(newItem)
+
+    items = domtree.getElementsByTagName("item")
+    print('There are {0} item tags'.format(items.length))
 
 if __name__ == '__main__':
     main()
