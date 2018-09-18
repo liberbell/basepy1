8313ef322687e267cb7315d4f3624d333bf89059
liberbell <liberbell@gmail.com>
2018-09-19 07:45:46 +0900

    domparse2

---
+++ domparse.py
@@ -0,0 +1,19 @@
+import xml.dom.minidom
+import requests
+
+def main():
+    url = 'http://httpbin.org/xml'
+    result = requests.get(url)
+
+    domtree = xml.dom.minidom.parseString(result.text)
+    rootnode = domtree.documentElement
+
+    print('The root Element is {0}'.format(rootnode.nodeName))
+    print('Title: {0}'.format(rootnode.getAttribute('title')))
+
+    items = domtree,getElementsByTagName('item')
+    print('There are {0} item tags'.formart(item.length))
+
+
+if __name__ == '__main__':
+    main()
