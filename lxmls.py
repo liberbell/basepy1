676e2a7350e9ae94f59e99b875adacae4d0e8620
liberbell <liberbell@gmail.com>
2018-09-19 17:05:48 +0900

    lxml3

---
+++ lxmls.py
@@ -0,0 +1,16 @@
+import requests
+from lxml import etree
+
+def main():
+    url = 'http://httpbin.org/xml'
+    result = requests.get(url)
+
+    doc = etree.fromstring(result.content)
+    print(result.text)
+
+    print(doc.tag)
+    print(doc.attrib['title'])
+
+
+if __name__ == '__main__':
+    main()
