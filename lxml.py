9ab824906ae2c17828c97fa2f5a73cbd330d4680
liberbell <liberbell@gmail.com>
2018-09-19 16:56:17 +0900

    lxml2

--- lxml.py
+++ lxml.py
@@ -5,6 +5,9 @@ def main():
     url = 'http://httpbin.org/xml'
     result = requests.get(url)
 
+    doc = etree.fromstring(result.content)
+    print(result.text)
+
 
 if __name__ == '__main__':
     main()
